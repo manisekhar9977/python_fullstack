f6bc4151dbe060f508b552e07a5b0f7f5908158a
Manisekhar9977 <manisekharchowdary9@gmail.com>
2026-07-28 16:18:49 +0530

    js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to NRIIT learning Management System")
+let heading = document.getElementById("welcome");
+heading.innerHTML = "welcome Future Software Engineers"
+console.log("heading element: ", heading)
---
+++ js/test.js
@@ -0,0 +1,21 @@
+console.log("hello world")
+let student_name = "mani"
+console.log("student name is: " + student_name)
+let student_age = 21
+let course_name = "Python Full Stack"
+console.log("Student age is:" + student_age, "Course name is:" + course_name)
+let fee = 10000
+let discount = 10
+let discounted_fee = fee - (fee * discount / 100)
+console.log("Discount fee is: " + discounted_fee)
+
+let age=18
+if(age>=18){
+    console.log("eligible for admisson")
+}
+else{
+    console.log("not eligible for admission")
+}
+for(let i=1;i<=5;i++){
+    console.log("iteration number:"+i)
+}
